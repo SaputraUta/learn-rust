53adaaf6a1b1c2c0ef0f1bff7beff7e3434738c3
Saputra <saputra.uta50@gmail.com>
2024-12-25 01:26:54 +0800

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,88 @@
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1
-}
-
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup <X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y
-        }
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
-fn main () {
-    let p1 = Point { x: 5, y: 10 };
-    let p2 = Point { x: "Hello", y: 'c' };
+pub struct Article {
+    pub title: String,
+    pub author: String,
+    pub content: String,
+}
 
-    let p3 = p1.mixup(p2);
+impl Summary for Article {
+    fn summarize(&self) -> String {
+        format!("{} by {}", self.title, self.author)
+    }
 
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{} by {}", self.content, self.username)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("{}", self.username)
+    }
+}
+
+pub struct NewsArticle {
+    pub title: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as I said before, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    let article = Article {
+        title: String::from("Penguins"),
+        author: String::from("Jane Doe"),
+        content: String::from("I saw penguins today"),
+    };
+
+    let news_article = NewsArticle {
+        title: String::from("Cats"),
+        author: String::from("John Doe"),
+        content: String::from("I saw cats today"),
+    };
+
+    notify(&tweet);
+    notify(&article);
+    notify(&news_article);
+
+    // println!("Tweet: {} ", tweet.summarize());
+    // println!("Article: {} ", article.summarize());
+    // println!("News Article: {} ", news_article.summarize());
+
+    // println!("Tweet: {} ", tweet.summarize_author());
+    // println!("Article: {} ", article.summarize_author());
+    // println!("News Article: {} ", news_article.summarize_author());
 }
