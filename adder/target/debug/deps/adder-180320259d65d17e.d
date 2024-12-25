5896996adb831dbff71f523598f3220bb12ff53e
Saputra <saputra.uta50@gmail.com>
2024-12-25 23:14:37 +0800

    how to write tests

---
+++ adder/target/debug/deps/adder-180320259d65d17e.d
@@ -0,0 +1,7 @@
+D:\Study\Rust\tutorial1\adder\target\debug\deps\libadder-180320259d65d17e.rmeta: src\lib.rs
+
+D:\Study\Rust\tutorial1\adder\target\debug\deps\libadder-180320259d65d17e.rlib: src\lib.rs
+
+D:\Study\Rust\tutorial1\adder\target\debug\deps\adder-180320259d65d17e.d: src\lib.rs
+
+src\lib.rs:
---
+++ adder/target/debug/deps/adder-5387a0c967e384f9.d
@@ -0,0 +1,5 @@
+D:\Study\Rust\tutorial1\adder\target\debug\deps\adder-5387a0c967e384f9.exe: src\lib.rs
+
+D:\Study\Rust\tutorial1\adder\target\debug\deps\adder-5387a0c967e384f9.d: src\lib.rs
+
+src\lib.rs:
